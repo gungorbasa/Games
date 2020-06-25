46a4ac1a34e8aebdd196cf9fec30d2bd5de3e65c
Gungor Basa <gungorbasa@gmail.com>
2020-06-25 10:08:27 +0300

    Alamofire Networking is added

---
+++ Games/Networking/AlamofireNetworking.swift
@@ -0,0 +1,47 @@
+//
+//  AlamofireNetworking.swift
+//  Games
+//
+//  Created by Gungor Basa on 6/25/20.
+//  Copyright © 2020 Gungor Basa. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+final class AlamofireNetwork: Networking {
+  func run<T>(route: Routing, completion: @escaping (Result<T, Error>) -> ()) where T : Decodable, T : Encodable {
+    let request = AF.request(
+      route.host,
+      method: convertMethod(method: route.method),
+      parameters: route.parameters,
+      encoding: URLEncoding(destination: .queryString),
+      headers: HTTPHeaders(route.headers)
+    )
+    request.validate()
+      .responseDecodable(of: T.self) { response in
+        guard let object = response.value else {
+          completion(.failure(response.error ?? HTTPResponseError.unknown))
+          return
+        }
+        completion(.success(object))
+    }
+  }
+
+  func cancel() {
+
+  }
+
+  private func convertMethod(method: HTTPMethod) -> Alamofire.HTTPMethod {
+    switch method {
+    case .get:
+      return .get
+    case .post:
+      return .post
+    case .put:
+      return .put
+    case .delete:
+      return .delete
+    }
+  }
+}
